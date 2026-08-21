690c97a942d510570d6ec8d4cba00d543c49758f
Exider <Bvkube95@gmail.com>
2026-08-21 17:46:31 +0200

    hslToHex function

--- src/renderer/settings.ts
+++ src/renderer/settings.ts
@@ -29,3 +29,45 @@ function hexToHsl(hex: string): { h: number, s: number, l: number } {
         l: l * 100 
     }
 }
+
+function hslToHex(h: number, s: number, l: number): string {
+  const sN = s / 100
+  const lN = l / 100
+  const c = (1 - Math.abs(2 * lN - 1)) * sN
+  const x = c * (1 - Math.abs(((h / 60) % 2) - 1))
+  const m = lN - c / 2
+  let r = 0
+  let g = 0
+  let b = 0
+  if (h < 60) {
+    r = c
+    g = x
+  } else if (h < 120) {
+    r = x
+    g = c
+  } else if (h < 180) {
+    g = c
+    b = x
+  } else if (h < 240) {
+    g = x
+    b = c
+  } else if (h < 300) {
+    r = x
+    b = c
+  } else {
+    r = c
+    b = x
+  }
+  const toHex = (v: number) =>
+    Math.round((v + m) * 255)
+      .toString(16)
+      .padStart(2, "0")
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`
+}
+
+interface AccentPalette {
+  accent700: string
+  accent500: string
+  accent400: string
+  accent300: string
+}
